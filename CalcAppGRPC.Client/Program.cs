734743dee23eed6b286f3019c180c96b40937ff8
Камчыбек Абдыназаров <tilekberdiev@outlook.com>
2021-08-06 06:37:33 +0600

    Добавьте файлы проекта.

---
+++ CalcAppGRPC.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace CalcAppGRPC.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var calcClient = new Calc.CalcClient(channel);
+            while (true)
+            {
+                Console.Write("Калькулятор\nA+B, A-B, A*B, A/B: ");
+                var response = await calcClient.ExecuteAsync(new CalcRequest { Value = Console.ReadLine() });
+                Console.WriteLine(response.Result);
+            }
+        }
+    }
+}
---
+++ CalcAppGRPC.Server/Services/CalcService.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace CalcAppGRPC.Server.Services
+{
+    public class CalcService : Calc.CalcBase
+    {
+        private async Task<CalcResponse> AdditionAsync(double[] value)
+        => new CalcResponse { Result = (value[0] + value[1]).ToString() };
+
+        private async Task<CalcResponse> DivisionAsync(double[] value)
+        => value[1] == 0 ? new CalcResponse { Result = "Делить на ноль нельзя!" } 
+        : new CalcResponse { Result = (value[0] / value[1]).ToString() };
+
+        private async Task<CalcResponse> MultiplicationAsync(double[] value)
+        => new CalcResponse { Result = (value[0] * value[1]).ToString() };
+
+        private async Task<CalcResponse> SubtractionAsync(double[] value)
+        => new CalcResponse { Result = (value[0] - value[1]).ToString() };
+
+        private async Task<char> FindOperatorAsync(string value)
+        {
+            if (value.Contains('+')) return '+';
+            if (value.Contains('-')) return '-';
+            if (value.Contains('*')) return '*';
+            if (value.Contains('/')) return '/';
+
+            return '_';
+        }
+        public override async Task<CalcResponse> Execute(CalcRequest request, ServerCallContext context)
+        => await FindOperatorAsync(request.Value) switch
+            {
+                '+' => await AdditionAsync(request.Value.Split('+').Select(double.Parse).ToArray()),
+                '-' => await SubtractionAsync(request.Value.Split('-').Select(double.Parse).ToArray()),
+                '*' => await MultiplicationAsync(request.Value.Split('*').Select(double.Parse).ToArray()),
+                '/' => await DivisionAsync(request.Value.Split('/').Select(double.Parse).ToArray()),
+                _ => new CalcResponse { Result = "Error" }
+            };
+    }
+}
